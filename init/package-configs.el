fe86519957fe834f9d36a3c7eb9671a0fa005036
luiguip <luisguilhermepdea@gmail.com>
2020-08-19 09:41:35 -0300

    Refactoration + Evil

--- init/package-configs.el
+++ init/package-configs.el
@@ -1,4 +1,5 @@
-(setq my-packages
+(let
+    ((my-packages
       '(ido-vertical-mode
         flx-ido
         smex
@@ -9,24 +10,25 @@
         rainbow-delimiters
         projectile
         popwin
-        flycheck))
+        flycheck
+        evil)))
 
-(dolist (package my-packages)
-  (unless (package-installed-p package)
-    (package-install package)))
-
-(setq my-configs
-      '("ido-config"
-        "smex-config"
-        "company-config"
-        "magit-config"
-        "paredit-config"
-        "rainbow-delimiters-config"
-        "projectile-config"
-        "popwin-config"
-        "flycheck-config"))
-
+  (dolist (package my-packages)
+    (unless (package-installed-p package)
+      (package-install package))))
 (add-to-list 'load-path "~/.emacs.d/init/package-configs")
 
+(let
+    ((my-configs
+       '("ido-config"
+         "smex-config"
+         "company-config"
+         "magit-config"
+         "paredit-config"
+         "rainbow-delimiters-config"
+         "projectile-config"
+         "popwin-config"
+         "flycheck-config"
+         "evil-config")))
 (dolist (config my-configs)
-  (load-library config))
+  (load-library config)))
