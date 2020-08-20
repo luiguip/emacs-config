83ceeed1cea6b82705c28e1d72b07f78b949cfae
luiguip <luisguilhermepdea@gmail.com>
2020-08-19 23:22:18 -0300

    lsp-ui config added

--- init/package-configs/lsp-config.el
+++ init/package-configs/lsp-config.el
@@ -1,6 +1,12 @@
 (require 'lsp-mode)
+
 (add-to-list 'load-path "~/.emacs.d/init/package-configs/lsp-config")
+
 (let ((lsp-configs
        '("lsp-elixir-config")))
   (dolist (lsp-config lsp-configs)
     (load-library lsp-config)))
+
+(require 'lsp-ui)
+(define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+(define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
