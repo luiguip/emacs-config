4a4e259059670fd116a609e6e2c643f2a0e37023
Luigui de A <luisguilhermepdea@gmail.com>
2020-08-27 17:53:35 -0300

    Refactored, no generated code anymore.

--- init.el
+++ init.el
@@ -3,8 +3,10 @@
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 ;;Removing custom file from init.el
-(setq custom-file "~/.emacs.d/generated.el")
-(load-file "~/.emacs.d/generated.el")
+(setq-default custom-file (expand-file-name ".generated.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+;;(load-file "~/.emacs.d/generated.el")
 ;;Configs
 (add-to-list 'load-path "~/.emacs.d/src")
 (load-library "configs")
